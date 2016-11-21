74e088ab1a3358ba8700097bd9d8f0abeabdc372
Pouya <pouyaheydary@gmail.com>
2016-11-21 13:01:17 +0330

    adds calendar widget to the app

---
+++ app/src/main/java/ir/apptune/calendar/AppWidget.java
@@ -0,0 +1,39 @@
+package ir.apptune.calendar;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+public class AppWidget extends AppWidgetProvider {
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+
+        for (int i=0; i<appWidgetIds.length ; i++){
+            int appWidgetId = appWidgetIds[i];
+            Log.d("LOG" , i+"");
+            Intent intent = new Intent(context,MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+            remoteViews.setOnClickPendingIntent(R.id.txt_widget_show_date, pendingIntent);
+            CalendarTool calendar = new CalendarTool();
+            String s =calendar.getWeekDayStr() + " "+ PersianNumberFormatHelper.toPersianNumber(calendar.getIranianDay()+"") + " "+
+        PersianMonthName.getName(calendar.getIranianMonth()) + " "+
+        PersianNumberFormatHelper.toPersianNumber(calendar.getIranianYear()+"");
+
+            remoteViews.setTextViewText(R.id.txt_widget_show_date, s);
+            Log.d("LOG" , calendar.getIranianDate()+"");
+
+            appWidgetManager.updateAppWidget(appWidgetId,remoteViews);
+
+
+        }
+    }
+}
