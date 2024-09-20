a94bbb05c1ae1c5068ac046472a35803cf690490
Pouya Heydari <pouya.heydari@sumup.com>
2024-09-20 20:19:26 +0200

    Removed unused peace of codes in ResourceUtils

--- core/src/main/java/com/pouyaheydari/calendar/core/utils/ResourceUtils.kt
+++ core/src/main/java/com/pouyaheydari/calendar/core/utils/ResourceUtils.kt
@@ -8,13 +8,6 @@ import org.xmlpull.v1.XmlPullParser
 import javax.inject.Inject
 import javax.inject.Singleton
 
-/**
- * This class returns Persian Calendar events , with parsing the xml's inside the app.
- * please look at xml folder
- */
-
-private const val XCalendarH = "ObservedHijriCalendar"
-private const val XCalendarG = "GregorianCalendar"
 private const val XCalendarP = "PersianCalendar"
 private const val EVENT = "Event"
 private const val TITLE = "Title"
@@ -24,21 +17,12 @@ private const val VACATION = "IsVacation"
 private const val X_CALENDAR = "XCalendar"
 private const val IS_VACATION = "1"
 
-/**
- * This class reads xml files that contain holidays dates
- */
 @Singleton
 class ResourceUtils @Inject constructor(app: Application) {
 
     var eventG = hashMapOf<Int, String>()
         private set
-    var eventH = hashMapOf<Int, String>()
-        private set
     var eventP = hashMapOf<Int, String>()
-        private set
-    var vacationG = hashMapOf<Int, Boolean>()
-        private set
-    var vacationH = hashMapOf<Int, Boolean>()
         private set
     var vacationP = hashMapOf<Int, Boolean>()
         private set
@@ -82,16 +66,6 @@ class ResourceUtils @Inject constructor(app: Application) {
                                     eventP[dayMonth].toString() + " " + title else eventP[dayMonth] =
                                     title
                                 if (vacation) vacationP[dayMonth] = true
-                            } else if (xCalendar == XCalendarH) {
-                                if (eventH.containsKey(dayMonth)) eventH[dayMonth] =
-                                    eventH[dayMonth].toString() + " " + title else eventH[dayMonth] =
-                                    title
-                                if (vacation) vacationH[dayMonth] = true
-                            } else if (xCalendar == XCalendarG) {
-                                if (eventG.containsKey(dayMonth)) eventG[dayMonth] =
-                                    eventG[dayMonth].toString() + " " + title else eventG[dayMonth] =
-                                    title
-                                if (vacation) vacationG[dayMonth] = true
                             }
                         }
                     }
