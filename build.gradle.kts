1f8590caec000111073b2b6ca7330833d34bcf5a
Pouya Heydari <pouya.heydari@sumup.com>
2024-10-02 16:57:37 +0200

    Removed unused kotlin.jvm dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.hiltGradle) apply false
     alias(libs.plugins.ksp) apply false
