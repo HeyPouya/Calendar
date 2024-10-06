f97c86dfc31b404f4a69c855027424ec3a4bd056
Pouya Heydari <pouya.heydari@sumup.com>
2024-10-06 13:46:49 +0200

    Changed button in DayDetailsBottomSheet to OutlinedButton

--- app/src/main/java/com/pouyaheydari/calendar/ui/components/DayDetailsBottomSheet.kt
+++ app/src/main/java/com/pouyaheydari/calendar/ui/components/DayDetailsBottomSheet.kt
@@ -5,11 +5,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -70,7 +70,7 @@ fun DayDetailsBottomSheet(
                     }
                 }
                 item {
-                    Button(
+                    OutlinedButton(
                         modifier = Modifier.padding(16.dp),
                         onClick = onSetReminderClicked
                     ) {
